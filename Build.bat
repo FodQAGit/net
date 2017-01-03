d92c0dc5f8f9a96d5dc9b452e542eac39b0ebc39
FodQAGit <fodqagit@gmail.com>
2017-01-03 17:30:42 +0200

    Add files via upload

---
+++ Build.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "C:\Users\afong\Desktop\PetShop_4.0\PetShop.sln" /t:Build /p:Configuration=Release
+PAUSE
---
+++ DecryptWebConfig.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\aspnet_regiis.exe -pdf "connectionStrings" "C:\Stuff\PetShop_4.0\Web"
+PAUSE
